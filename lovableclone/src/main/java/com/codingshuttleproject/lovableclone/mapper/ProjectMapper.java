479955c66c06f0bec772de2df797da2ba972277d
Mani5746 <srivastavmanish389@gmail.com>
2026-05-05 23:38:55 +0530

    Making Get User Repository and making Hot Reload Configuration

--- lovableclone/src/main/java/com/codingshuttleproject/lovableclone/mapper/ProjectMapper.java
+++ lovableclone/src/main/java/com/codingshuttleproject/lovableclone/mapper/ProjectMapper.java
@@ -5,6 +5,7 @@ import com.codingshuttleproject.lovableclone.dto.project.ProjectResponse;
 import com.codingshuttleproject.lovableclone.dto.project.ProjectSummaryResponse;
 import com.codingshuttleproject.lovableclone.entity.Project;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public interface ProjectMapper {
 
     ProjectResponse toProjectResponse(Project project);
 
+    @Mapping(target = "projectName",source = "name")
     ProjectSummaryResponse toProjectSummaryResponse(Project project);
 
     List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
