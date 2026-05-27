169adb606463dcbb7d69be74cf7b145dfc5b2cf3
Mani5746 <srivastavmanish389@gmail.com>
2026-05-27 23:35:06 +0530

    Adding Login Feature using JWT Token

--- lovableclone/src/main/java/com/codingshuttleproject/lovableclone/service/impl/AuthServiceImpl.java
+++ lovableclone/src/main/java/com/codingshuttleproject/lovableclone/service/impl/AuthServiceImpl.java
@@ -7,10 +7,14 @@ import com.codingshuttleproject.lovableclone.entity.User;
 import com.codingshuttleproject.lovableclone.errors.BadRequestException;
 import com.codingshuttleproject.lovableclone.mapper.UserMapper;
 import com.codingshuttleproject.lovableclone.repository.UserRepository;
+import com.codingshuttleproject.lovableclone.security.AuthUtil;
 import com.codingshuttleproject.lovableclone.service.AuthService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,20 +26,28 @@ public class AuthServiceImpl implements AuthService {
     UserRepository userRepository;
     UserMapper userMapper;
     PasswordEncoder passwordEncoder;
+    AuthUtil authUtil;
+    AuthenticationManager authenticationManager;
+
     @Override
     public AuthResponse signup(SignupRequest request) {
         userRepository.findByUsername(request.username()).ifPresent(user->{
                 throw new BadRequestException("User already exists with username:"+request.username());
         });
-
         User user = userMapper.toEntity(request);
        user.setPassword(passwordEncoder.encode(request.password()));
        userRepository.save(user);
-        return new AuthResponse("dummy",userMapper.toUserProfileResponse(user));
+       String token= authUtil.generateAccessToken(user);
+        return new AuthResponse(token,userMapper.toUserProfileResponse(user));
     }
 
     @Override
     public AuthResponse login(LoginRequest request) {
-        return null;
+        Authentication authentication=authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.username(),request.password())
+        );
+        User user=(User) authentication.getPrincipal();
+        String token=authUtil.generateAccessToken(user);
+        return new AuthResponse(token, userMapper.toUserProfileResponse(user));
     }
 }
--- lovableclone/src/main/java/com/codingshuttleproject/lovableclone/service/impl/UserServiceImpl.java
+++ lovableclone/src/main/java/com/codingshuttleproject/lovableclone/service/impl/UserServiceImpl.java
@@ -1,15 +1,33 @@
 package com.codingshuttleproject.lovableclone.service.impl;
 
 import com.codingshuttleproject.lovableclone.dto.auth.UserProfileResponse;
+import com.codingshuttleproject.lovableclone.errors.ResourceNotFoundException;
+import com.codingshuttleproject.lovableclone.repository.UserRepository;
 import com.codingshuttleproject.lovableclone.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+     UserRepository userRepository;
+
 
     @Override
     public UserProfileResponse getProfile(Long userId) {
-        // TODO: implement get profile logic
+
         return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()-> new ResourceNotFoundException("user",username));
     }
 }
